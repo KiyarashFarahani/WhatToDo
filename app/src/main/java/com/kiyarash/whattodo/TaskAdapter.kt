db86f2147a3eee4b6f53420389fde7b37e1212b7
kiyarash <farahanikiyarash@gmail.com>
2024-07-27 11:46:12 +0330

    implement recyclerview

---
+++ app/src/main/java/com/kiyarash/whattodo/TaskAdapter.kt
@@ -0,0 +1,40 @@
+package com.kiyarash.whattodo
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Adapter
+import android.widget.CheckBox
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemAdapter(private val data: List<Task>): RecyclerView.Adapter<ItemAdapter.ViewHolder>(){
+	class ViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
+		val taskName: TextView = view.findViewById(R.id.taskNameTextView)
+		val due: TextView = view.findViewById(R.id.dueTextView)
+		val isDone: CheckBox = view.findViewById(R.id.isDoneCheckBox)
+	}
+
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+		val layout = LayoutInflater
+			.from(parent.context)
+			.inflate(R.layout.item_view, parent, false)
+
+		return ViewHolder(layout)
+	}
+
+	override fun getItemCount(): Int {
+		return data.size
+	}
+
+	override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+		val	item = data[position]
+		holder.taskName.text = item.taskName
+		holder.due.text = buildString {
+			append(item.dueDate.toString())
+			append(" ")
+			append(item.dueTime.toString())
+		}
+		holder.isDone.isChecked = item.isDone
+	}
+}
